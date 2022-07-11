ea58b7aab414f8e94fb328c9c312a7eb90b3205f
ajs3801 <tt14an@gmail.com>
2022-07-11 13:56:56 +0900

    modify data collection to save flip video

--- CustomTrain/DataSet_collection/collect.py
+++ CustomTrain/DataSet_collection/collect.py
@@ -11,8 +11,8 @@ import uuid
 DATA_PATH = os.path.join('Data') 
 
 # Actions that we try to detect
-# actions = np.array(['lunge-down','lunge-up','crunch-down','crunch-up'])
-actions = np.array(['lunge-down','lunge-up'])
+actions = np.array(['squat-down','squat-up','pushup-down','pushup-up','lunge-down','lunge-up','crunch-down','crunch-up'])
+
 # Thirty videos worth of data
 no_sequences = 5
 
@@ -37,51 +37,48 @@ for action in actions:
         fps = cap.get(cv2.CAP_PROP_FPS)
         fps = int(fps)
 
-        fps_low = int(fps * 0.7)
-        fps_high = int(fps * 1.2)
-
-        #                                       운동번호 일련번호 번호 프레임
-        videopath = os.path.join(DATA_PATH,action,'{}-{}-{}-{}.avi'.format(str('0'+str(action_num)),datetime.today().strftime('%Y%m%d%H%M'),str(sequence),str(fps)))
-        videopath_low = os.path.join(DATA_PATH,action,'{}-{}-{}-{}.avi'.format(str('0'+str(action_num)),datetime.today().strftime('%Y%m%d%H%M'),str(sequence),str(fps_low)))
-        videopath_high = os.path.join(DATA_PATH,action,'{}-{}-{}-{}.avi'.format(str('0'+str(action_num)),datetime.today().strftime('%Y%m%d%H%M'),str(sequence),str(fps_high)))
+        #                                       운동번호 일련번호 번호 프레임 flip(true or false)
+        videopath = os.path.join(DATA_PATH,action,'{}-{}-{}-{}-{}.avi'.format(str('0'+str(action_num)),datetime.today().strftime('%Y%m%d%H%M'),str(sequence),str(fps).str(0)))
+        videopath_flip = os.path.join(DATA_PATH,action,'{}-{}-{}-{}-{}.avi'.format(str('0'+str(action_num)),datetime.today().strftime('%Y%m%d%H%M'),str(sequence),str(fps),str(1)))
 
         out = cv2.VideoWriter(videopath, fourcc, fps, (width,height))
-        out_low = cv2.VideoWriter(videopath_low, fourcc, fps_low, (width,height))
-        out_high = cv2.VideoWriter(videopath_high, fourcc, fps_high, (width,height))
+        out_flip = cv2.VideoWriter(videopath_flip, fourcc, fps, (width,height))
 
         print('Collecting frames for {} Video Number {}'.format(action, sequence))
         for frame_num in range(sequence_length):
             ret, frame = cap.read()
+            show_frame = frame.copy()
+
+            print('Collecting frames for {} Video Number {}'.format(action, sequence))
+            cv2.putText(show_frame,'Collecting frames for {} Video Number {}'.format(action, sequence), (50,50), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
+            cv2.imshow('Realtime view',show_frame)
             # NEW Apply wait logic
             if frame_num == 0: 
                 print('STARTING COLLECTION for {} wait...'.format(action))
-                cv2.imshow('Data collection', frame)
+                # cv2.imshow('Data collection', frame)
                 cv2.waitKey(5000)
                 # Show to screen
 
                 out.write(frame) # 영상데이터만 저장 (소리 X)
-                out_low.write(frame)
-                out_high.write(frame)
+                out_flip.write(frame)
 
             else: 
                 # Show to screen
-                cv2.imshow('Data collection', frame)
+                # cv2.imshow('Data collection', frame)
                 out.write(frame) # 영상데이터만 저장 (소리 X)
-                out_low.write(frame)
-                out_high.write(frame)
+                out_flip.write(frame)
 
             # Break gracefully
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
         
         print('save to ', videopath)
-        print('save to ', videopath_low)
-        print('save to ', videopath_high)
-        cv2.waitKey(2000)
+        print('save to ', videopath_flip)
+
+        cv2.waitKey(1000)
     action_num += 1
 
 out.release()
-out_high.release()
-out_low.release()
+out_flip.release()
 cap.release()
 cv2.destroyAllWindows()
